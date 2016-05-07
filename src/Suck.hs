a85c397eb64692e8a98baf5bc503f136eb862a03
Kevin Freese <kevin.m.freese@gmail.com>
2016-05-06 22:29:03 -0500

    suck functional

--- src/Suck.hs
+++ src/Suck.hs
@@ -2,11 +2,15 @@ import Network.Browser (browse, request, setOutHandler)
 import Network.HTTP (getRequest)
 import Network.HTTP.Base (rspBody)
 import Text.HTML.TagSoup
-import qualified Data.Map as Map
 import System.Environment (getArgs)
 
 import Control.Monad (forM_)
 import Control.Monad.State
+import qualified Data.Map as Map
+import Data.Function (on)
+import Data.Ord
+import Data.List (sortBy)
+import Data.Maybe
 
 type PrimitiveModel = Map.Map (String,String) [String]
 type FreqModel = Map.Map (String,String) [(Int,String)]
@@ -25,10 +29,25 @@ mkPrimModel words = mkPrimModel' ("","") words Map.empty
     mkPrimModel' state [] map = map
 
 simplifyModel :: PrimitiveModel -> FreqModel
-simplifyModel m = undefined
+simplifyModel m = Map.map toFreq m
+  where
+    toFreq l = sortBy (compare `on` (Down . fst)) $ map (\(a,b) -> (b,a)) $ 
+               Map.toList $ toFreq' Map.empty l
 
-processModel :: FreqModel -> ProcessedModel
-processModel m = undefined
+    toFreq' :: Map.Map String Int -> [String] -> Map.Map String Int
+    toFreq' accum [] = accum
+    toFreq' accum (w:ws) = toFreq' (Map.insertWith (+) w 1 accum) ws
+
+mkPrcModel :: FreqModel -> ProcessedModel
+mkPrcModel m = map processState $ Map.toList m
+  where 
+    processState ((x,y),freqs) = (y, mapMaybe (mkIdx y) freqs)
+    mkIdx y (i,str) = do
+      idx <- Map.lookupIndex (y,str) m
+      return (i,idx)
+
+process :: [String] -> ProcessedModel
+process = mkPrcModel . simplifyModel . mkPrimModel
 
 harvestText :: [Tag String] -> [String]
 harvestText tags = concatMap words $ harvest' False tags
@@ -54,5 +73,5 @@ main = do
        request $ getRequest url
     return $ (harvestText . parseTags . rspBody) resp
   let allWords = concat rawWords
-  let prcModel = (processModel . simplifyModel . mkPrimModel) allWords
+  let prcModel = process allWords
   mapM_ (appendFile outFile . (++ "\n") . show) prcModel 
